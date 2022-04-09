4108724ffd0672a03b18c2d18bee1521cc0ab45d
HicaroD <hdanrlley1@gmail.com>
2022-04-09 02:45:22 -0300

    Adding links to `dotfiles` and personal notes

--- script.py
+++ script.py
@@ -1,7 +1,12 @@
 from os import system
 
+NOTES_URL = "https://github.com/HicaroD/Notes"
+DOTFILES_URL = "https://github.com/HicaroD/dotfiles-neovim"
+
+
 def main():
     system("echo Hello, world!")
+
 
 if __name__ == "__main__":
     main()
