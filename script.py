3fe81c14129c797190a48d5d440e007690e89f1a
HicaroD <hdanrlley1@gmail.com>
2022-04-09 21:51:37 -0300

    [FEAT] Builder class and methods for installing base packages

--- script.py
+++ script.py
@@ -4,8 +4,31 @@ NOTES_URL = "https://github.com/HicaroD/Notes"
 DOTFILES_URL = "https://github.com/HicaroD/dotfiles-neovim"
 
 
+class Builder():
+    @staticmethod
+    def install_package(package_name: str):
+        system(f"sudo pacman -S {package_name}")
+
+    @staticmethod
+    def update_system():
+        system("sudo pacman -Syu")
+
+    @staticmethod
+    def install_base_packages():
+        packages = ["git", "python", "zsh", "neovim"]
+        for package in packages:
+            Builder.install_package(package)
+
+        # Oh My Zsh
+        system("sh -c \"$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)\"")
+
+    @staticmethod
+    def run():
+        Builder.update_system()
+        Builder.install_base_packages()
+
 def main():
-    system("echo Hello, world!")
+    Builder.run()
 
 
 if __name__ == "__main__":
