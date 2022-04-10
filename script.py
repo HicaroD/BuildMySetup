1f61c650b07f981591d277100abb422410487c72
HicaroD <hdanrlley1@gmail.com>
2022-04-09 22:04:35 -0300

    [Format] Applying `black` code formatter

--- script.py
+++ script.py
@@ -4,7 +4,7 @@ NOTES_URL = "https://github.com/HicaroD/Notes"
 DOTFILES_URL = "https://github.com/HicaroD/dotfiles-neovim"
 
 
-class Builder():
+class Builder:
     @staticmethod
     def install_package(package_name: str):
         system(f"sudo pacman -S {package_name}")
@@ -20,12 +20,15 @@ class Builder():
             Builder.install_package(package)
 
         # Oh My Zsh
-        system("sh -c \"$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)\"")
+        system(
+            'sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"'
+        )
 
     @staticmethod
     def run():
         Builder.update_system()
         Builder.install_base_packages()
+
 
 def main():
     Builder.run()
